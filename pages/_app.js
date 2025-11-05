4754c89c7fa6998499d88010ce860fff1e1d968b
SolGood <129625770+solgood44@users.noreply.github.com>
2025-11-05 12:30:56 -0500

    Add Google Analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../web/styles.css';
 import './styles.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9CDHCMHT8J"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9CDHCMHT8J');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
